bf4db505126b2f49133b83215822efddee553c54
Stuart Ingram <ingrams2@upmc.com>
2020-04-21 12:27:16 -0400

    Working TODO Rest service

---
+++ server/spring-boot-vue/src/main/java/com/srai/springbootvue/Todo.java
@@ -0,0 +1,20 @@
+package com.srai.springbootvue;
+
+import lombok.*;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Todo {
+
+  @Id @GeneratedValue
+  private Long id;
+
+  @NonNull
+  private String title;
+
+  private Boolean completed = false;
+}
