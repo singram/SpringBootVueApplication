997b9616d5fc01572fe6e0f890a292c965f319b2
Stuart Ingram <ingrams2@upmc.com>
2020-04-22 15:12:43 -0400

    Added 'Todo' client application

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SpringBootVueApplication
 SpringBoot + Vue tutorial and experiment jump point
+
+#References
+https://dzone.com/articles/build-a-secure-app-with-spring-boot-and-vuejs
+https://github.com/moksamedia/okta-spring-boot-vue-client
+https://github.com/moksamedia/okta-spring-boot-vue-server
+
+Vue notes of interest
+[v-cloak] -> https://medium.com/vuejs-tips/v-cloak-45a05da28dc4
+
+Alternative SpringBoot/Vue tutorial
+https://jaxlondon.com/blog/java-core-languages/put-spring-boot-und-vue-js-practical-use-project-tutorial/
+
+Handling CORS
+https://spring.io/guides/gs/rest-service-cors/
+Securing Spring Boot
+https://developer.okta.com/blog/2018/07/30/10-ways-to-secure-spring-boot
+SpringBoot Authorization server
+https://docs.spring.io/spring-security-oauth2-boot/docs/current/reference/html/boot-features-security-oauth2-authorization-server.html
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos />
+    <footer class="info">
+      <p>Based on a project written by <a href="http://evanyou.me">Evan You</a></p>
+      <p>Original Vue TodoApp project is <a href="https://vuejs.org/v2/examples/todomvc.html">here</a></p>
+      <p>Modified for this tutorial by Andrew Hughes</p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Todos from './components/Todos'
+  // app Vue instance
+  const app = {
+    name: 'app',
+    components: {
+      Todos
+    },
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    // app initial state
+    data: () => {
+      return {
+      }
+    }
   }
-}
+  export default app
 </script>
 
+/* Before Vue mount into the page, you can see the unparsed {{tags}} for a fraction of a second.
+v-cloak is a attribute that you can add to a element you want to hide, while Vue is mounting. */
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  [v-cloak] { display: none; }
 </style>
